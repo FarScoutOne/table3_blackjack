46bff04b21eb58e4a12f57be01203c1dd4608e7a
Ryne <64247887+FarScoutOne@users.noreply.github.com>
2021-08-02 23:12:14 -0700

    Remove pending questions and issue comments
    I added these as issues in the GitHub repository instead as a more appropriate location to deal with them.

--- terminal_blackjack.py
+++ terminal_blackjack.py
@@ -2,16 +2,6 @@
 # by: Ryne Smith
 # started on: 8/1/2021
 
-'''
-This is a simple game of blackjack that plays in the command-line terminal.
-There are still a few aspects that need to be decided:
-- Will more than one player be able to play against the dealer/computer?
-- Will the deck be shuffled/randomized and prior to a hand, or will the only one card be determined at a time? (Schrodinger's deck)
-- How many hands can be used with one deck?
-- Will a manual be provided?
-- How will scoring be determined? (a point per hand won vs betting)
-'''
-
 # Greeting
 print("Welcome to table 3.")
 print("Please, have a seat.")
